b49ef0f61e30b53acc2fdfeaa2df633694239bbd
DaGestapo <105201337+DaGestapo@users.noreply.github.com>
2023-04-16 12:00:39 +0300

    all

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,45 @@ const path = require('path');
 const common = require('./webpack.common');
 const {merge} = require('webpack-merge');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TersertPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        filename: "main.[contenthash].js",
+        filename: "[name].[contenthash].bundle.js",
         path: path.join(__dirname, 'dist')
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TersertPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            }),
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename:"[name].[contenthash].css"
+        }),
+    ],
+    module:{
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader"
+            ]
+        }]
+    }
 });
